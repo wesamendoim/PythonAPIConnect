af06839baa2a4772119c6a4101f92d22a69311c3
wesamendoim <wesleyvbnardino@hotmail.com>
2023-10-09 09:04:12 -0300

    API IBGE

--- ConnAPIBancoCentral.py
+++ ConnAPIBancoCentral.py
@@ -1,11 +1,15 @@
 #URL API Banco Central https://dadosabertos.bcb.gov.br/dataset?res_format=API
-
 #Sintaxe da API https://olinda.bcb.gov.br/olinda/servico/mecir_moedas_comemorativas/versao/v1/odata/[codigo_recurso]?$format=json&[Outros Parâmetros]
 
+# LINK IBGE https://servicodados.ibge.gov.br/api/docs
+
+# https://servicodados.ibge.gov.br/api/v3/agregados/2259/periodos/201601/variaveis/1076?localidades=N7[3501]
+
+import ssl
 import requests
 
-link = 'https://olinda.bcb.gov.br/olinda/servico/mecir_dinheiro_em_circulacao/versao/v1/odata/informacoes_diarias?$top=10000&$orderby=Data%20desc&$format=json'
+link = "https://servicodados.ibge.gov.br/api/v3/agregados/2259/periodos/201601/variaveis/1076?localidades=N7[3501]"
 
 requisicao = requests.get(link)
 
-print(requisicao)
+print(requisicao.json)()

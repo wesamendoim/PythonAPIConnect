22abc936df6fc83903d14cee9006eb7d66aa2f46
wesamendoim <wesleyvbnardino@hotmail.com>
2023-10-11 14:27:38 -0300

    DataFrame JSON

--- ConnAPIBancoCentral.py
+++ ConnAPIBancoCentral.py
@@ -6,12 +6,17 @@
 
 # API BANCO CENTRAL "https://olinda.bcb.gov.br/olinda/servico/Informes_PostosDeAtendimento/versao/v1/odata/PostosAtendimento?$top=100&$format=json&$select=Cnpj,NomeIf,Segmento,NomePosto,TipoPosto,Endereco,Numero,Complemento,Bairro,Cep,MunicipioIbge,Municipio,UF,DDD,Telefone,CnpjAssist,NomeAssist,Posicao"
 
+import  pprint
 import requests
+import pandas as pd
 
 link = "https://olinda.bcb.gov.br/olinda/servico/Informes_PostosDeAtendimento/versao/v1/odata/PostosAtendimento?$top=100&$format=json&$select=Cnpj,NomeIf,Segmento,NomePosto,TipoPosto,Endereco,Numero,Complemento,Bairro,Cep,MunicipioIbge,Municipio,UF,DDD,Telefone,CnpjAssist,NomeAssist,Posicao"
 
 requisicao = requests.get(link, verify=False)
 
-print(requisicao.json())
+jsLista = requisicao.json()
 
-print(requests.status_codes)
+strDataFrame = pd.DataFrame(jsLista['value'])
+
+print(strDataFrame)
+

1d512383a965d071ebc1374aaaf41cb05997d2ee
wesamendoim <wesleyvbnardino@hotmail.com>
2023-11-20 09:44:34 -0300

    Arquivo principal

--- Principal.py
+++ Principal.py
@@ -20,10 +20,14 @@ try:
     cursor = conexao.cursor()
     for index, row in strDataFrame.iterrows():
 
-        strQuery = """INSERT INTO TBL_POSTO_ATENDIMENTO (CNPJ, NOMEIF, SEGMENTO,  NOMEPOSTO,TIPOPOSTO, ENDERECO,NUMERO, COMPLEMENTO, BAIRRO, CEP, MUNICIPIOIBGE, MUNICIPIO, UF, DDD, TELEFONE, CNPJASSIST, NOMEASSIST, POSICAO) VALUES (%s,%s%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
-        #strValores = ('Cnpj','NomeIf','Segmento','NomePosto','TipoPosto','Endereco','Numero','Complemento','Bairro','Cep','MunicipioIbge','Municipio','UF','DDD','Telefone','CnpjAssist','NomeAssist','Posicao')
-        strValores = row['Cnpj'], row['NomeIf'], row['Segmento'], row['NomePosto'], row['TipoPosto'], row['Endereco'],row['Numero'],row['Complemento'],row['Bairro'], row['Cep'],row['MunicipioIbge'],row['Municipio'],row['UF'],row['DDD'],row['Telefone'],row['CnpjAssist'],row['NomeAssist'], row['Posicao']
-        cursor.execute(strQuery, strValores)
+        strValores = row['Cnpj'], row['NomeIf'], row['Segmento'], row['NomePosto'], row['TipoPosto'], row['Endereco'], \
+        row['Numero'], row['Complemento'], row['Bairro'], row['Cep'], row['MunicipioIbge'], row['Municipio'], row['UF'], \
+        row['DDD'], row['Telefone'], row['CnpjAssist'], row['NomeAssist'], row['Posicao']
+
+        strQuery = ("INSERT INTO TBL_POSTO_ATENDIMENTO (CNPJ, NOMEIF, SEGMENTO, NOMEPOSTO,TIPOPOSTO, ENDERECO,NUMERO, COMPLEMENTO, BAIRRO, CEP, MUNICIPIOIBGE, MUNICIPIO, UF, DDD, TELEFONE, CNPJASSIST, NOMEASSIST, POSICAO) "
+                    "VALUES (%s,%s%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", strValores)
+
+        cursor.execute(strQuery)
         cursor.commit()
 
 

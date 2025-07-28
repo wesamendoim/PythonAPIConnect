4f20347660f1cb7491eaadda8c385113fc1d456a
wesleyvbnardino <wesleyvbnardino@hotmail.com>
2025-07-28 17:03:50 -0300

    DADOS INSERIDOS NO BANCO

--- Criacao_Banco.py
+++ Criacao_Banco.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, create_engine
+from sqlalchemy import Column, String, Integer, create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 db = create_engine('sqlite:///APIBancoCentral.db')
@@ -9,8 +9,9 @@ session = Session()
 Base = declarative_base()
 
 class TBL_POSTO_ATENDIMENTO(Base):
-    __tablename__ = 'TBL_POSTO_ATENDIMENTO'
-    CNPJ = Column("CNPJ", String,primary_key=True)
+    __tablename__ = 'TBL_POSTO_ATENDIMENTO_V2'
+    ID = Column(Integer, primary_key=True, autoincrement=True)
+    CNPJ = Column("CNPJ", String)
     NOMEIF = Column("NOMEIF", String)
     SEGMENTO = Column("SEGMENTO", String(100))
     NOMEPOSTO = Column("NOMEPOSTO", String(100))
@@ -29,7 +30,7 @@ class TBL_POSTO_ATENDIMENTO(Base):
     NOMEASSIST = Column("NOMEASSIST", String(100))
     POSICAO = Column("POSICAO", String(100))
 
-    def __init__(self, CNPJ,NOMEIF, SEGMENTO, NOMEPOSTO, TIPOPOSTO, ENDERECO, NUMERO, COMPLEMENTO, BAIRRO, CEP, MUNICIPIOIBGE, MUNICIPIO, UF, DDD, TELEFONE, CNPJASSIST,NOMEASSIST,POSICAO):
+    def __init__(self,  CNPJ,NOMEIF, SEGMENTO, NOMEPOSTO, TIPOPOSTO, ENDERECO, NUMERO, COMPLEMENTO, BAIRRO, CEP, MUNICIPIOIBGE, MUNICIPIO, UF, DDD, TELEFONE, CNPJASSIST,NOMEASSIST,POSICAO):
         self.CNPJ = CNPJ
         self.NOMEIF = NOMEIF
         self.SEGMENTO = SEGMENTO
--- Principal.py
+++ Principal.py
@@ -12,44 +12,31 @@ import openpyxl
 
 try:
     strRetorno = ConnAPIBancoCentral.retorno_Json()
-    pprint.pprint(strRetorno)
-    print(type(strRetorno))
     #strDataFrame = pd.read_excel("strDataFrame.xlsx")
     strRetorno_df = pd.DataFrame(strRetorno)
     pprint.pprint(strRetorno_df)
     linhasJson = strRetorno_df.to_dict(orient='records')
+    print(linhasJson.append(strRetorno_df['value']))
 
+    # Supondo que strRetorno_df já existe e tem a coluna 'value'
+    tabela = pd.json_normalize(strRetorno_df['value'])
 
-    for dados in linhasJson:
+    # Agora 'tabela' é um DataFrame com cada campo do dicionário como coluna
+    print(tabela.head())
+
+    # Se quiser salvar como Excel:
+    tabela.to_excel("strDataFrame.xlsx", index=False)
+
+    strDataFrame = pd.read_excel("strDataFrame.xlsx")
+
+    for index, row in strDataFrame.iterrows():
             #xiste = Criacao_Banco.session.query(Criacao_Banco.TBL_POSTO_ATENDIMENTO).filter_by(CNPJ=row['Cnpj']).first()
-                    print(dados)
+                    print(row)
             #if not existe:
-                #posto_atendimento = Criacao_Banco.TBL_POSTO_ATENDIMENTO(CNPJ=row['Cnpj'], NOMEIF=row['NomeIf'], SEGMENTO=row['Segmento'], NOMEPOSTO=row['NomePosto'], TIPOPOSTO=row['TipoPosto'], ENDERECO=row['Endereco'], NUMERO=row['Numero'], COMPLEMENTO=row['Complemento'], BAIRRO=row['Bairro'], CEP=row['Cep'], MUNICIPIOIBGE=row['MunicipioIbge'], MUNICIPIO=row['Municipio'], UF=row['UF'], DDD=row['DDD'], TELEFONE=row['Telefone'], CNPJASSIST=row['CnpjAssist'], NOMEASSIST=row['NomeAssist'], POSICAO=row['Posicao'])
-                    posto_atendimento = Criacao_Banco.TBL_POSTO_ATENDIMENTO(
-                        CNPJ=dados.get['Cnpj'],
-                        NOMEIF=dados.get('NomeIf'),
-                        SEGMENTO=dados.get('Segmento'),
-                        NOMEPOSTO=dados.get('NomePosto'),
-                        TIPOPOSTO=dados.get('TipoPosto'),
-                        ENDERECO=dados.get('Endereco'),
-                        NUMERO=dados.get('Numero'),
-                        COMPLEMENTO=dados.get('Complemento'),
-                        BAIRRO=dados.get('Bairro'),
-                        CEP=dados.get('Cep'),
-                        MUNICIPIOIBGE=dados.get('MunicipioIbge'),
-                        MUNICIPIO=dados.get('Municipio'),
-                        UF=dados.get('UF'),
-                        DDD=dados.get('DDD'),
-                        TELEFONE=dados.get('Telefone'),
-                        CNPJASSIST=dados.get('CnpjAssist'),
-                        NOMEASSIST=dados.get('NomeAssist'),
-                        POSICAO=dados.get('Posicao')
-                    )
-
+                    posto_atendimento = Criacao_Banco.TBL_POSTO_ATENDIMENTO(CNPJ=row['Cnpj'], NOMEIF=row['NomeIf'], SEGMENTO=row['Segmento'], NOMEPOSTO=row['NomePosto'], TIPOPOSTO=row['TipoPosto'], ENDERECO=row['Endereco'], NUMERO=row['Numero'], COMPLEMENTO=row['Complemento'], BAIRRO=row['Bairro'], CEP=row['Cep'], MUNICIPIOIBGE=row['MunicipioIbge'], MUNICIPIO=row['Municipio'], UF=row['UF'], DDD=row['DDD'], TELEFONE=row['Telefone'], CNPJASSIST=row['CnpjAssist'], NOMEASSIST=row['NomeAssist'], POSICAO=row['Posicao'])
                     print(posto_atendimento)
-    Criacao_Banco.session.add(posto_atendimento)
+                    Criacao_Banco.session.add(posto_atendimento)
 
-          
     Criacao_Banco.session.commit()
     print("Dados inseridos com sucesso!")
 

fa0c4bec70d58a5c69bec05ee92e8911930867db
wesleyvbnardino <wesleyvbnardino@hotmail.com>
2025-07-28 15:57:53 -0300

    Inserindo no banco

--- Principal.py
+++ Principal.py
@@ -1,7 +1,7 @@
 #Arquivo principal
 
 import ConnAPIBancoCentral
-import ConexaoBancoDeDados
+#import ConexaoBancoDeDados
 import Criacao_Banco
 import pprint
 import pandas as pd
@@ -13,27 +13,49 @@ import openpyxl
 try:
     strRetorno = ConnAPIBancoCentral.retorno_Json()
     pprint.pprint(strRetorno)
-    Conexao = ConexaoBancoDeDados.conexaoBanco()
-    Conexao._open_connection()
-    strDataFrame = pd.read_excel("strDataFrame.xlsx")
-    #strDataFrame = wb.load_workbook("strDataFrame.xlsx")
+    print(type(strRetorno))
+    #strDataFrame = pd.read_excel("strDataFrame.xlsx")
+    strRetorno_df = pd.DataFrame(strRetorno)
+    pprint.pprint(strRetorno_df)
+    linhasJson = strRetorno_df.to_dict(orient='records')
 
-    #engine = db.create_engine( 'mssql+pyodbc://@' + server_name + '/' + database_name + '?trusted_connection=yes&driver=' + driver, fast_executemany=True)
-    #strDataFrame.to_sql(con=conexao, name='TBL_POSTO_ATENDIMENTO', if_exists='replace', index=False)
 
-    for index, row in strDataFrame.iterrows():
-            posto_atendimento = Criacao_Banco.TBL_POSTO_ATENDIMENTO(CNPJ=row['Cnpj'], NOMEIF=row['NomeIf'], SEGMENTO=row['Segmento'], NOMEPOSTO=row['NomePosto'], TIPOPOSTO=row['TipoPosto'], ENDERECO=row['Endereco'], NUMERO=row['Numero'], COMPLEMENTO=row['Complemento'], BAIRRO=row['Bairro'], CEP=row['Cep'], MUNICIPIOIBGE=row['MunicipioIbge'], MUNICIPIO=row['Municipio'], UF=row['UF'], DDD=row['DDD'], TELEFONE=row['Telefone'], CNPJASSIST=row['CnpjAssist'], NOMEASSIST=row['NomeAssist'], POSICAO=row['Posicao'])
-            Conexao.session.add(posto_atendimento)
-            Conexao.session.commit()
+    for dados in linhasJson:
+            #xiste = Criacao_Banco.session.query(Criacao_Banco.TBL_POSTO_ATENDIMENTO).filter_by(CNPJ=row['Cnpj']).first()
+                    print(dados)
+            #if not existe:
+                #posto_atendimento = Criacao_Banco.TBL_POSTO_ATENDIMENTO(CNPJ=row['Cnpj'], NOMEIF=row['NomeIf'], SEGMENTO=row['Segmento'], NOMEPOSTO=row['NomePosto'], TIPOPOSTO=row['TipoPosto'], ENDERECO=row['Endereco'], NUMERO=row['Numero'], COMPLEMENTO=row['Complemento'], BAIRRO=row['Bairro'], CEP=row['Cep'], MUNICIPIOIBGE=row['MunicipioIbge'], MUNICIPIO=row['Municipio'], UF=row['UF'], DDD=row['DDD'], TELEFONE=row['Telefone'], CNPJASSIST=row['CnpjAssist'], NOMEASSIST=row['NomeAssist'], POSICAO=row['Posicao'])
+                    posto_atendimento = Criacao_Banco.TBL_POSTO_ATENDIMENTO(
+                        CNPJ=dados.get['Cnpj'],
+                        NOMEIF=dados.get('NomeIf'),
+                        SEGMENTO=dados.get('Segmento'),
+                        NOMEPOSTO=dados.get('NomePosto'),
+                        TIPOPOSTO=dados.get('TipoPosto'),
+                        ENDERECO=dados.get('Endereco'),
+                        NUMERO=dados.get('Numero'),
+                        COMPLEMENTO=dados.get('Complemento'),
+                        BAIRRO=dados.get('Bairro'),
+                        CEP=dados.get('Cep'),
+                        MUNICIPIOIBGE=dados.get('MunicipioIbge'),
+                        MUNICIPIO=dados.get('Municipio'),
+                        UF=dados.get('UF'),
+                        DDD=dados.get('DDD'),
+                        TELEFONE=dados.get('Telefone'),
+                        CNPJASSIST=dados.get('CnpjAssist'),
+                        NOMEASSIST=dados.get('NomeAssist'),
+                        POSICAO=dados.get('Posicao')
+                    )
 
-# strValores = row['Cnpj'], row['NomeIf'], row['Segmento'], row['NomePosto'], row['TipoPosto'], row['Endereco'], \
-# row['Numero'], row['Complemento'], row['Bairro'], row['Cep'], row['MunicipioIbge'], row['Municipio'], row['UF'], \
-# row['DDD'], row['Telefone'], row['CnpjAssist'], row['NomeAssist'], row['Posicao']
+                    print(posto_atendimento)
+    Criacao_Banco.session.add(posto_atendimento)
 
-# strQuery = ("INSERT INTO TBL_POSTO_ATENDIMENTO (CNPJ, NOMEIF, SEGMENTO, NOMEPOSTO,TIPOPOSTO, ENDERECO,NUMERO, COMPLEMENTO, BAIRRO, CEP, MUNICIPIOIBGE, MUNICIPIO, UF, DDD, TELEFONE, CNPJASSIST, NOMEASSIST, POSICAO) "
-#             "VALUES (%s,%s%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", strValores)
-       
+          
+    Criacao_Banco.session.commit()
+    print("Dados inseridos com sucesso!")
+
 
 except Exception as Ex:
     print(Ex)
+finally:
+    Criacao_Banco.session.close()
     #print(Ex.args.index())

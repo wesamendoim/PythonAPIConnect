dd129604a5a85d9cb699ab631e8bc23ce0cf5dd2
wesleyvbnardino <wesleyvbnardino@hotmail.com>
2025-07-30 15:43:25 -0300

    aRQUIVO txt

--- test_Aplicacao.py
+++ test_Aplicacao.py
@@ -2,5 +2,5 @@ import ConnAPIBancoCentral
 
 def test_retorno_Json():
     resultado = ConnAPIBancoCentral.retorno_Json()
-    assert isinstance(resultado, dict)  # Verifica se o retorno é um dicionário
-    assert 'resultados' in resultado or len(resultado) > 0  # Verifica se há dados
+    assert isinstance(resultado, dict) 
+    assert 'resultados' in resultado or len(resultado) > 0

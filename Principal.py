3328b9bd749f46016b2ce1a7231579fdc44b890a
wesleyvbnardino <wesleyvbnardino@hotmail.com>
2025-07-30 10:07:36 -0300

    Adicionada condição para não inserir dados duplicados

--- Principal.py
+++ Principal.py
@@ -30,16 +30,18 @@ try:
     strDataFrame = pd.read_excel("strDataFrame.xlsx")
 
     for index, row in strDataFrame.iterrows():
-            #xiste = Criacao_Banco.session.query(Criacao_Banco.TBL_POSTO_ATENDIMENTO).filter_by(CNPJ=row['Cnpj']).first()
-                    print(row)
-            #if not existe:
+            #Verificar se o CNPJ já existe na tabela
+            existe = Criacao_Banco.session.query(Criacao_Banco.TBL_POSTO_ATENDIMENTO).filter_by(CNPJ=row['Cnpj']).first()
+            print(existe)
+
+            # Se o CNPJ não existir, insira os dados
+            if not existe:
                     posto_atendimento = Criacao_Banco.TBL_POSTO_ATENDIMENTO(CNPJ=row['Cnpj'], NOMEIF=row['NomeIf'], SEGMENTO=row['Segmento'], NOMEPOSTO=row['NomePosto'], TIPOPOSTO=row['TipoPosto'], ENDERECO=row['Endereco'], NUMERO=row['Numero'], COMPLEMENTO=row['Complemento'], BAIRRO=row['Bairro'], CEP=row['Cep'], MUNICIPIOIBGE=row['MunicipioIbge'], MUNICIPIO=row['Municipio'], UF=row['UF'], DDD=row['DDD'], TELEFONE=row['Telefone'], CNPJASSIST=row['CnpjAssist'], NOMEASSIST=row['NomeAssist'], POSICAO=row['Posicao'])
                     print(posto_atendimento)
                     Criacao_Banco.session.add(posto_atendimento)
 
     Criacao_Banco.session.commit()
-    print("Dados inseridos com sucesso!")
-
+    
 
 except Exception as Ex:
     print(Ex)
